1e24a4ff13cefff9a26b15a1a2d119a3719b6bd3
Kire_minsoo <79205662+ViolinSoloist@users.noreply.github.com>
2025-10-24 11:57:00 -0300

    BubbleSort, Struct
    Implementação de BubbleSort otimizado, Definição de struct para guardar informações após ordenação (vetor ordenado, tempo de execução, comparações e swaps)
    Também já introduzi como podemos realizar as medições dessas informações, usando BubbleSort como exemplo.
    
    Também já testei alguns exemplos diferentes de tamanho e modo, e aparentemente parece estar dando tudo certo, mas
    ### Testar para diferentes tamanhos de vetores e diferentes modos (registros aleatórios, ordenados e inversamente ordenados)
    
    (Algoritmo de BubbleSort otimizado e o uso das structs estão todos corretos)

--- main.c
+++ main.c
@@ -5,16 +5,12 @@
 #include <time.h>     
 
 #define MIN_RAND 1
-#define MAX_RAND 69420
+#define MAX_RAND 100000
 
-//  acho que a gente não precisa fazer uma função pros vetores
-//  a gente pdoe criar eles com malloc, no caso os 4 vetores de 100 1000 e tal e resetar o valor deles pra cada sort
-//  
-// :(( VOU ESCREVER TUDO EM BRAINFUCK MESMO FODASE >...++-->;>><<..<++-->>>.__ (HLO WORLD BLYAT)
+#define ui unsigned int
 
-// sim sim, a função seria só pra deixar mais claro, mas seria só pra gerar esses 4 vetores, em vez de deixar na main
 /**
- * @attention a gente vai precisar, em todos os sorts, criar e cuidar de contadores de:
+ * @attention em todos os sorts, criar e cuidar de contadores de:
  * número de comparações
  * número de movimentações de registro
  * tempo de execução (usar biblioteca <time.h>)
@@ -55,18 +51,29 @@
 */
 
 
+// ------------------------------------------
+//            FUNÇÕES AUXILIARES
+// ------------------------------------------
 /**
- * @attention AINDA NÃO TESTEI
+ * @note testado, mas ainda assim é melhor testar com mais exemplos
  * @attention tem que ter a mesma quantidade de free()'s que tem essa função chamada na main
+ * @details como devemos testar cada método de ordenação com diferentes quantidades de vetores com diferentes tamanhos e modos,
+ * é inteligente fazer uma função que trate a geração desses vetores
+ * recebe o tamanho do vetor @param n e o @param modo de geração (aleatorio, ordenado e inversamente ordenado)
+ * @return ponteiro pra array de inteiros alocados dinamicamente.
  */
 int* make_vector(int n, int modo) // 1 -> aleatorio, 2 -> ordenado 3 -> inverso
 {
     int* v = (int*)calloc(n, sizeof(int)); // v inicia com 0 em todos index, eu acho
-    if (v != NULL) {
-        // melhor fazer o tratamento de erro pro algo que for chamar essa função, em vez de fazer aqui mesmo
-        // porque se for erro ele vai retornar o vetor que é NULL
-        
+    if (v != NULL)
+    {    
         switch (modo)
+//  acho que a gente não precisa fazer uma função pros vetores
+//  a gente pdoe criar eles com malloc, no caso os 4 vetores de 100 1000 e tal e resetar o valor deles pra cada sort
+//  
+// :(( VOU ESCREVER TUDO EM BRAINFUCK MESMO FODASE >...++-->;>><<..<++-->>>.__ (HLO WORLD BLYAT)
+
+// sim sim, a função seria só pra deixar mais claro, mas seria só pra gerar esses 4 vetores, em vez de deixar na main
         {
             case 1: //aleatorio
                 // TODO <stdlib.h> rand() preencher com números aleatórios
@@ -99,6 +106,18 @@ int* make_vector(int n, int modo) // 1 -> aleatorio, 2 -> ordenado 3 -> inverso
     return v;
 }
 
+/**
+ * @brief função que realiza swap entre dois registros de um vetor
+ * recebe o vetor, e as posições em index (0-indexado) que serão trocadas de lugar
+ * @
+ */
+void swap(int* v, int a, int b)
+{
+    int temp = v[a];
+    v[a] = v[b];
+    v[b] = temp;
+}
+
 void print_vetor(int* v, int n) {
     for(int i=0; i<n; i++) {
         printf("%d ", v[i]);
@@ -106,13 +125,76 @@ void print_vetor(int* v, int n) {
     printf("\n");
 }
 
-int main()
+
+/**
+ * $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ *                 FUNÇÕES DE ORDENAÇÃO 
+ * $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ * 
+ * @details cada função de ordenação @returns uma struct, contendo:
+ * - ponteiro para vetor pós-ordenação (alocado dinamicamente)
+ * - inteiro que representa o número de comparações realizadas
+ * - inteiro representando número de movimentações de registros (provavelmente swaps?)
+ * - float representando tempo de execução até terminar a ordenação
+ * 
+ * @attention uso de struct não é necessário, mas ajuda a modularizar o código
+ * @deprecated por enquanto, é só isso, mas é importante lembrar que a gente vai ter que
+ * guardar todas essas informações para todos os tamanhos de vetores e para cada tamanho, 
+ * todas as formas de registros originais (ordenado, aleatório, inversamente ordenado)
+*/
+
+typedef struct info_ {
+    int* sorted_array;
+    ui comparisons;
+    ui swaps;
+    float execution_time;
+}Info;
+
+
+Info bubble_sort(int* v, int n)
 {
-    int* vetor = make_vector(10, 3);
+    ui comparisons = 0;
+    ui swaps = 0;
+    clock_t inicio = clock(); //inicio da marcação de tempo
+
+
+    for(int i=0; i<n-1; i++)
+    {
+        bool teve_swap = false;
+
+        for(int j=0; j<n-1-i; j++)
+        {          
+            comparisons++; // if que verifica se o registro atual é maior que o próximo registro
+            if(v[j] > v[j+1]) {
+                swaps++; // sempre que tem swap incrimenta
+                swap(v, j, j+1);
+                teve_swap = true;
+            }
+        }
+        if (!teve_swap) break; //se nenhum movimento é feito quer dizer que já está ordenado
+    }
+
+    clock_t fim = clock(); // fim da marcação de tempo
+    double tempo_gasto = (double)(fim - inicio) / CLOCKS_PER_SEC;
+
+    Info bbs_info = {v, comparisons, swaps, tempo_gasto};
+    return bbs_info;
+}
+
+
+int main()
+{   
+    // NOTAS PARA DANILO: TÁ FUNCINOANDO AGORA! TESTE COM DIFERENTES TAMANHOS DE N,
+    int n = 10000;
+    int* vetor = make_vector(n, 1); //TESTE TAMBÉM COM DIFERENTES TIPOS DE DE MODOS (1, 2 OU 3) (ALEATORIO, ORDENADO, INVERSAMENTE ORDENADO)
     
-    print_vetor(vetor, 10);
+    // print_vetor(vetor, n);
+
+    Info bbs = bubble_sort(vetor, n);
+
+    // print_vetor(bbs.sorted_array, n);
+    printf("Comparacoes: %u, swaps: %u, tempo decorrido: %.3lf %s.\n", bbs.comparisons, bbs.swaps, (bbs.execution_time < 0.1 ? bbs.execution_time * 1000 : bbs.execution_time), (bbs.execution_time < 0.1 ? "ms" : "s"));
 
     free(vetor);
-
     return 0;
 }
